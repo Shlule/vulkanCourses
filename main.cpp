533b8e248fde032381bff7efca497ba138f00608
Rogouan <elouan.rogliano@gmail.com>
2024-08-17 21:11:18 +0200

    add glm lib

--- main.cpp
+++ main.cpp
@@ -1,45 +1,32 @@
-#include <vulkan/vulkan.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
+#include <GLFW/glfw3.h>
 
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
+int main(){
+    std::cout << "hey, Zues\n";
+
+    GLFWwindow *window;
+
+    if (!glfwInit()){
+        fprintf(stderr, "failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
     }
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
-
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
+    window = glfwCreateWindow(300,300, "Gears", NULL, NULL);
+    if(!window){
+        fprintf(stderr, "failed to open GLFW window\n");
         glfwTerminate();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    VkInstance instance;
-    VkApplicationInfo appInfo{};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Vulkan";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    while (!glfwWindowShouldClose(window)){
+        // draw();
 
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
+        // animate();
 
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-        std::cerr << "Failed to create Vulkan instance" << std::endl;
-        return -1;
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
     }
-
-    std::cout << "Vulkan instance created successfully!" << std::endl;
-
-    vkDestroyInstance(instance, nullptr);
-    glfwDestroyWindow(window);
     glfwTerminate();
 
-    return 0;
 }
